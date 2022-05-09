3bacec8dabfd11be703eda4c7f7e690d672e5f06
Paul Jankowski <pauljankowski@Teeadmin1279s-MacBook-Pro.local>
2022-05-09 06:08:51 -0400

    * Fixes argument type errors * Remoeves eslint plugin declaration

--- utils/algolia.ts
+++ utils/algolia.ts
@@ -3,7 +3,10 @@ import algoliasearchServer from "algoliasearch";
 
 import { ALGOLIA_APP_ID } from "./constants";
 
-export default function createSearchClient(apiKey: string, isServer: boolean) {
+export default function createSearchClient(
+  apiKey: string,
+  isServer: boolean = false
+) {
   return isServer
     ? algoliasearchServer(ALGOLIA_APP_ID, apiKey)
     : algoliasearch(ALGOLIA_APP_ID, apiKey);
